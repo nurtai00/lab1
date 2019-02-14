c5e0dfbfa8e22f54cbd74742af3773cf4be66124
nurtai00 <nnn.nurtai00@gmail.com>
2019-02-14 21:04:46 +0600

    first commit

---
+++ Task1/L1_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace L1_1
+{
+    class Program
+    {
+        static bool isPrime(int n)
+        {
+            if (n == 1) return false;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int n = int.Parse(s);
+            string k = Console.ReadLine();
+            string[] arr = k.Split(' ');
+            int cnt = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (isPrime(int.Parse(arr[i])))
+                {
+                    cnt++;
+                }
+            }
+            Console.WriteLine(cnt);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int num = int.Parse(arr[i]);
+                if (isPrime(num))
+                {
+                    Console.Write(num);
+                    Console.Write(' ');
+                }
+            }
+        }
+    }
+}
---
+++ Task3/L1_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace L1_3
+{
+    class Program
+    {
+        static int[] arrMaker(string[] arr)
+        {
+            int cnt = -1;
+            int[] array = new int[arr.Length * 2];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                cnt++;
+                array[cnt] = int.Parse(arr[i]);
+                cnt++;
+                array[cnt] = int.Parse(arr[i]);
+            }
+            return array;
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int n = int.Parse(s);
+            string k = Console.ReadLine();
+            string[] arr = k.Split();
+            int[] array = arrMaker(arr);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                Console.Write(' ');
+            }
+        }
+    }
+}
